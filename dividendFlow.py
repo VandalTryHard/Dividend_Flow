e6a2f8e599fc4ce4e0dd5379c8563ea931a3ac2a
Gulenok Valentin <gulenok.valentin@yandex.ru>
2021-11-30 22:53:51 +0600

    Dividend_Flow V0.4
    1) Show stocks in portfolio +
    2) Add a share +
    3) Delete share +
    4) Change stock -
    5) Select investment interval (in days)
    6) Show total and graph ?(development)?
    7) Exit +

--- dividendFlow.py
+++ dividendFlow.py
@@ -1,6 +1,6 @@
 """(RU) Данная программа рассчитывает сумму дивидентов
 """
-
+import matplotlib.pyplot as plt
 import pandas as pd
 import plotly as pl
 import numpy as np
@@ -108,7 +108,6 @@ def changeStock():
 
 # 5
 def selectInterval():
-    # today = int(dt.today().strftime('%d'))
     fileStock_Interval = csv.reader(open("fileStock.csv"))
     rows = list(fileStock_Interval)
     nameTicker = input("Enter Name (ticker): ")
@@ -129,7 +128,53 @@ def selectInterval():
 
 
 def showResults():
-    print("showResults")
+    import math
+    today = dt.today().strftime('%d/%m/%y')
+    value_Share = float(df["|Share value|"][1])
+    numbers = int(df["|Number|"][1])
+    payout_Last = float(df["|Last payout %|"][1])
+    payment_once_every = int(df["|Payment once every (days)|"][1])
+    term_Inv = int(df["|Investment term|"][1])
+
+    terms = []
+    for t in range(0, term_Inv):
+        terms.append(t)
+
+    payments = []
+    for p in range(0, term_Inv):
+        p = float(p / payment_once_every)
+        payments.append(p)
+
+    values = []
+    for v in range(0, term_Inv):
+        v = float((((value_Share * numbers) * payout_Last / 100) * 70) / 100)  # с учетом налога так как недвига
+        values.append(v)
+
+    value = []
+    for v in values:
+        # реализовать изменение дохода от времени!!!!!!!!!!!!
+
+    x = terms
+    y = value
+    plt.plot(x, y)
+    plt.show()
+
+    # violate = (value_Share*numbers)*payout_Last/100
+    # term =
+    # print(violate)
+    # x = np.arange(0, 1, 1)
+    # y = np.arange(0, violate, 0.5)
+    # plt.plot(x, y)
+    # plt.xlabel("angle")
+    # plt.ylabel("sine")
+    # plt.title('sine wave')
+    # plt.show()
+
+    # for i in range(0, term_Inv):
+    #     x = i + 1
+    #     y = value_Share
+    #     x, y = [x, y]
+    #     mp.show(x, y)
 
 
 main()
